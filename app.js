7a0170748cbcbb9cc8f028cc3250e33c8794e849
Ratemo Lewis <retemolewis@gmail.com>
2023-05-03 19:42:18 +0300

    initial commit,data is stored in an array

---
+++ app.js
@@ -0,0 +1,52 @@
+const express=require('express');
+const bodyParser= require('body-parser');
+const date = require(__dirname+"/date.js");
+
+const app= express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+const items=["meditate","code for two hours"];
+const workItems=[];
+
+app.get("/",function(req, res){
+    
+const day=date.getDate();
+
+    res.render("list",{listTitle:day,newItem:items});
+
+});
+
+
+app.post("/",function(req, res){
+    
+ const item=req.body.newItem;
+ if(req.body.list==="Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+ }else{
+    items.push(item);
+    res.redirect("/");
+ }
+
+
+})
+
+app.get("/work", function(req, res){
+    res.render("list",{listTitle:"Work List",newItem:workItems});
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+
+
+
+
+
+app.listen(3000, function(res, req){
+    console.log('Server is running on port 3000');
+})
